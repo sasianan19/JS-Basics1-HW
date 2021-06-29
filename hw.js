3b43090b982540726cdbc9c3cb291bc44d0564a8
sasianan19 <sasianan19@gmail.com>
2021-06-29 15:15:55 -0600

    first commit

---
+++ hw.js
@@ -0,0 +1,43 @@
+//Input: integer or double/float
+var num1 = 45;
+var num2 = 62.1;
+var sum = num1 + num2;
+//Output: sum
+console.log(sum);
+//Edge case: any data type but an integer or double/float
+
+var diff = num1 - num2;
+//Output: difference
+console.log(diff);
+//Edge case: "
+
+var q = num1 / num2;
+//Output: quotient
+console.log(q);
+//Edge case: "
+
+var prod = num1 * num2;
+//Output: product
+console.log(prod);
+//Edge case: "
+
+
+
+//Input: two strings
+var word1 = "Hello";
+var word2 = " World";
+//Output: Hello World
+console.log(word1 + word2);
+//Input: concatenated string
+var word3 = "Hello World";
+//Edge case: any data type but strings
+
+
+
+//Input: strings
+var food = "My favorite food is ";
+var fav = "rice.";
+var favFood = food + fav;
+//Output: My favorite food is rice.
+console.log(favFood);
+//Edge case: any data type but strings
